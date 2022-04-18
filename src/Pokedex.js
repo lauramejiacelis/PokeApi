e751f2882621f5bfc6e6d863b43ad587884c6cf6
Laura Mejía Celis <lauram2celis@gmail.com>
2022-04-18 17:23:22 -0500

    homework advance

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,34 +2,30 @@ import { Component } from "react";
 import styles from "./Pokedex.module.css"
 import { fetchPokedex } from "./services";
 import { withRouter } from "./withRouter";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Badge } from 'reactstrap';
 
 class Pokedex extends Component {
     state= {
-        pokemonsInfo: []
+        pokemonsInfo: [],
+        pokemonName: "",
+        pokemonId : "",
+        pokemonImage : "",
+        pokemonAbilities: [],
+        pokemonTypes : []
     }
 
     componentDidMount() {
-        fetchPokedex().then((result)=>result.map(({id, name})=>{
-            console.log(id)
-            return {
-                id,
-                name
-            }
-        }))
-        //fetchPokemon().then((pokemons)=>{this.setState({pokemons})})
-
-        // .then((pokemonsInfo)=> pokemonsInfo.map(({abilities, id, name,sprites})=>{
-        //     let image = sprites.other.dream_world.front_default;
-        //     let abilitiesName = abilities.ability.name;
-        //     return{
-        //         image,
-        //         id,
-        //         name,
-        //         abilitiesName
-        //     }
-
-        // })
-        // )
+        fetchPokedex(7).then((result)=>{
+            this.setState({
+                pokemonName : result.name, //string
+                pokemonId : result.id,//numero
+                pokemonImage : result.sprites.other.dream_world.front_default,//string
+                pokemonAbilities : result.abilities,//object or array?,
+                pokemonTypes : result.types
+            })
+            console.log(`Nombre pokemon ${this.state.pokemonName}`)
+        })
     }
 
     render() {
@@ -37,7 +33,10 @@ class Pokedex extends Component {
             <div>
                 <h3>Pokedex</h3>
                 <div className={styles.pokemonInfoContainer}>
-
+                    <h4>No.{this.state.pokemonId} {this.state.pokemonName}</h4>
+                    <img src={this.state.pokemonImage} alt=""/>
+                    <h6>Type: </h6>{this.state.pokemonTypes.map(({type})=><Badge color="warning" pill>{type.name}</Badge>)}
+                    <h6>Abilities: </h6>{this.state.pokemonAbilities.map(({ability})=><Badge color="info" pill>{ability.name}</Badge>)}
                 </div>
             </div>
         )
--- src/services.js
+++ src/services.js
@@ -4,9 +4,23 @@ export function fetchPokemon() {
         .then(({ results }) => results.map(({name}) => name));
 }
 
-export function fetchPokedex() {
-    return fetch(`https://pokeapi.co/api/v2/pokemon/1`)
+export function fetchPokedex(id) {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
         .then((res)=> res.json())
+        
+        
+        // .then((result)=>{
+        //     let pokemonName = result.name; //string
+        //     let pokemonId = result.id;//numero
+        //     let pokemonImage = result.sprites.other.dream_world.front_default;//string
+        //     let pokemonAbilities = result.abilities;//object or array?
+        //     return {
+        //         pokemonName,
+        //         pokemonId,
+        //         pokemonImage,
+        //         pokemonAbilities
+        //     }
+        // })
         
         // .then((pokemonsInfo)=> pokemonsInfo.map(({abilities, id, name,sprites})=>{
         //     let image = sprites.other.dream_world.front_default;
